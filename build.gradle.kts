7d1b895511b50d357f12facd238035c21c46ca8f
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-05-10 19:30:32 +0200

    GEM-64 | PR fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,10 +95,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-crypto")
 
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-
     implementation(libs.kotlinlogging)
     implementation(libs.lib.gem)
     implementation(libs.bundles.resilience4j)
